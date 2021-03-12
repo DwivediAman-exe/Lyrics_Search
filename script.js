a8b97ba93bafc9849c58883b3d682086b9974296
Aman Dwivedi <19dwivediaman@gmail.com>
2021-03-13 00:18:12 +0530

    output fetched content

--- script.js
+++ script.js
@@ -8,7 +8,24 @@ const apiURL = "https://api.lyrics.ovh";
 // show data
 
 function showData(data) {
-    
+    let output = "";
+
+    data.data.forEach(song => {
+        output += `
+        <li>
+            <span>
+            <strong>${song.artist.name}</strong> - ${song.title}
+            </span>
+            <button class="btn" data-artist="${song.artist.name} data-songTitle="${song.title}">Get Lyrics</button>
+        </li>
+        `;
+    });
+
+    result.innerHTML = `
+        <ul class="songs">
+            ${output}
+        </ul>
+    `;
 }
 
 // Seacrh function
