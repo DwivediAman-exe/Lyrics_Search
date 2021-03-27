06b6837327b5b590f1f71d845a98584f106c67b7
Aman Dwivedi <57112545+born69confused@users.noreply.github.com>
2021-03-27 20:34:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function showData(data) {
 
 // Get prev and next songs
 async function getMoreSongs(url) {
-  const res = await fetch(`https://github.com/Rob--W/cors-anywhere/${url}`);
+  const res = await fetch(`${url}`);
   const data = await res.json();
 
   showData(data);
